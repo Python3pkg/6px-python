118a92ea23dbf3b551d1154749ba8de1af49fc57
Dustin Higginbotham <dustinh052@gmail.com>
2014-07-07 12:52:56 -0600

    Proper update.  Mutliple inputs and outputs

--- examples/resize.py
+++ examples/resize.py
@@ -1,20 +1,18 @@
-import imp
+from _6px import PX
 
-px = imp.load_source('PX', '../px/__init__.py')
-
-PX = px.PX.init(
+px = PX.init(
     api_key="***API_KEY***",
     api_secret="***API_SECRET***",
     user_id="***USER_ID***"
 )
 
-PX.load('taxi', './images/unsplash_city_taxi.jpg')
-PX.load('logo', 'http://6px.io/img/px-logo-md@2x.png')
+px.load('taxi', './images/unsplash_city_taxi.jpg')
+px.load('logo', 'http://6px.io/img/px-logo-md@2x.png')
 
-out = PX.output({ 'taxi': False })
+out = px.output({ 'taxi': False })
 
 out.resize({ 'width': 250 })
 out.tag('taxi')
 out.url('6px')
 
-PX.save()
+px.save()
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+import os
 
 setup(
 	name='6px',
-	version='0.0.1',
+	version='0.0.8',
 	author='6px',
 	author_email='ops@6px.io',
-	packages=['px'],
+	packages=['_6px'],
 	url='http://pypi.python.org/pypi/6px/',
 	license='LICENSE',
 	description='6px python sdk',
-	long_description=open('README.md').read(),
+	long_description=open(os.path.abspath('./README.md')).read(),
 )
