cb016ea53a06c8aa6eda8cb7b4869b0aaa57e0ed
Dustin Higginbotham <dustinh052@gmail.com>
2014-07-08 11:31:09 -0600

    Renamed README from txt to rst

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
 	name='px',
-	version='0.0.15',
+	version='0.0.16',
 	author='6px',
 	author_email='ops@6px.io',
 	packages=['_6px'],
     url='https://github.com/6px-io/6px-python',
 	license='LICENSE',
 	description='6px python sdk',
-	long_description=open('README.txt').read(),
+	long_description=open('README.rst').read(),
 )
